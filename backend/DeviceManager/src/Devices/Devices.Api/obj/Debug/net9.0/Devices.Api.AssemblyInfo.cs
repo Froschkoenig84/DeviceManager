039a85e2616a1bf6fe9daa08528e23d5dddad333
m.nicolai <froschkoenig84@web.de>
2026-01-16 07:15:08 +0100

    restyling - modern overview page - skretched detail page (like wireframe)

--- backend/DeviceManager/src/Devices/Devices.Api/obj/Debug/net9.0/Devices.Api.AssemblyInfo.cs
+++ backend/DeviceManager/src/Devices/Devices.Api/obj/Debug/net9.0/Devices.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Devices.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09bf8fe7ed9b809838fffc18d239885ef337558f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4511bfbc06f828e3c85a7f5da254f4553a9d099a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Devices.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Devices.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
